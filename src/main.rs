1299ab8a0e02582f01e99383a6ea20638528ade0
lesguillemets <kgkacko+github@gmail.com>
2025-03-14 20:12:56 +0900

    dealing with sha256

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use sha2::{Digest, Sha256};
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+fn do_main() -> io::Result<()> {
+    for f in fs::read_dir(".")? {
+        println!("{f:?}");
+        let entry = f?;
+        println!("{:?}", entry.file_type());
+        println!("{:?}", entry.metadata());
+        if entry.file_type()?.is_file() {
+            let mut f = File::open(entry.path())?;
+            let mut hasher = Sha256::new();
+            let mut b = Vec::new();
+            f.read_to_end(&mut b)?;
+            hasher.write_all(&b)?;
+            let result = hasher.finalize();
+            println! {"{:x}", result};
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    do_main().unwrap();
 }
