4ac528c6a49792c5f5a77b80dcefc2f91cfbcd83
lesguillemets <kgkacko+github@gmail.com>
2025-03-18 22:13:48 +0900

    split files

---
+++ src/lib.rs
@@ -0,0 +1,156 @@
+use sha2::{Digest, Sha256};
+use std::ffi::OsString;
+use std::fs::{DirEntry, File, Metadata};
+use std::io;
+use std::io::prelude::*;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Clone, Debug)]
+pub struct FileMeta {
+    len: u64,
+    created: Option<SystemTime>,
+    last_modified: Option<SystemTime>,
+    last_accessed: Option<SystemTime>,
+}
+
+impl FileMeta {
+    fn from_metadata(m: &Metadata) -> Self {
+        let len = m.len();
+        let created = m.created().ok();
+        let last_modified = m.modified().ok();
+        let last_accessed = m.accessed().ok();
+        FileMeta {
+            len,
+            created,
+            last_modified,
+            last_accessed,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct FileInfo {
+    e: DirEntry,
+    meta: FileMeta,
+    sha256: String,
+}
+
+impl FileInfo {
+    pub fn from_entry(e: DirEntry) -> io::Result<Self> {
+        if e.file_type()?.is_file() {
+            let meta = FileMeta::from_metadata(&e.metadata()?);
+            let sha256 = calc_sha256(e.path())?;
+            Ok(FileInfo { e, meta, sha256 })
+        } else if e.file_type()?.is_dir() {
+            Err(io::Error::new(
+                io::ErrorKind::IsADirectory,
+                "Is a directory",
+            ))
+        } else {
+            Err(io::Error::other("Not a file or directory"))
+        }
+    }
+
+    fn file_name(&self) -> OsString {
+        self.e.file_name()
+    }
+
+    fn path(&self) -> io::Result<PathBuf> {
+        self.e.path().canonicalize()
+    }
+
+    fn parent_and_parent_parent(&self) -> [Option<OsString>; 2] {
+        let p = self.e.path();
+        let comps: Vec<_> = p.components().collect();
+        let l = comps.len();
+        [
+            l.checked_sub(2)
+                .and_then(|i| comps.get(i))
+                .map(|&c| c.as_os_str().to_owned()),
+            l.checked_sub(3)
+                .and_then(|i| comps.get(i))
+                .map(|&c| c.as_os_str().to_owned()),
+        ]
+    }
+
+    pub fn header(sep: &str) -> String {
+        [
+            "file_name",
+            "path",
+            "parent_dir",
+            "parent_parent",
+            "size",
+            "created",
+            "modified",
+            "accessed",
+            "sha256",
+        ]
+        .join(sep)
+    }
+
+    pub fn write_csvline<W: Write>(&self, paper: &mut W) -> io::Result<()> {
+        let file_name = self.file_name().conv_to_string();
+        let path = self
+            .path()
+            .map(|v| v.conv_to_string())
+            .unwrap_or_else(|e| format!("{e:?}"));
+        let [p, pp] = self.parent_and_parent_parent();
+        let parent_dir = p.map(|pat| pat.conv_to_string()).unwrap_or("".to_string());
+        let parent_parent = pp.map(|pat| pat.conv_to_string()).unwrap_or("".to_string());
+        let size = self.meta.len.to_string();
+        let meta = &self.meta;
+        let [created, modified, accessed] = [meta.created, meta.last_modified, meta.last_accessed]
+            .map(|m| {
+                m.map_or(String::from(""), |c| {
+                    c.duration_since(UNIX_EPOCH).unwrap().as_secs().to_string()
+                })
+            });
+        let sha256 = self.sha256.clone();
+        let out = [
+            file_name,
+            path,
+            parent_dir,
+            parent_parent,
+            size,
+            created,
+            modified,
+            accessed,
+            sha256,
+        ]
+        .join(",");
+        writeln!(paper, "{out}")
+    }
+}
+
+fn calc_sha256<P: AsRef<Path>>(p: P) -> io::Result<String> {
+    let mut f = File::open(p)?;
+    let mut hasher = Sha256::new();
+    let mut b = Vec::new();
+    f.read_to_end(&mut b)?;
+    hasher.write_all(&b)?;
+    let result = hasher.finalize();
+    Ok(format!("{:x}", result))
+}
+
+pub trait ConvToString {
+    fn conv_to_string(self) -> String;
+}
+
+impl ConvToString for OsString {
+    fn conv_to_string(self) -> String {
+        self.to_string_lossy().to_string()
+    }
+}
+impl ConvToString for PathBuf {
+    fn conv_to_string(self) -> String {
+        self.to_string_lossy().to_string()
+    }
+}
+
+impl ConvToString for String {
+    fn conv_to_string(self) -> String {
+        self
+    }
+}
