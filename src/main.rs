a031fe93e71c00a2cc2a2111d89cbd01dac65964
lesguillemets <kgkacko+github@gmail.com>
2025-03-19 16:27:33 +0900

    stash: gradually handling command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::fs;
 use std::io;
 use std::io::{BufWriter, Write};
+use std::path::PathBuf;
+
+use clap::Parser;
 
 use kagirfilelists::FileInfo;
 
-fn do_main() -> io::Result<()> {
+fn do_main(dir: PathBuf) -> io::Result<()> {
     let mut w = BufWriter::new(io::stdout());
-    for f in fs::read_dir(".")? {
+    for f in fs::read_dir(dir)? {
         println!("{f:?}");
         let entry = f?;
         if entry.file_type()?.is_file() {
@@ -23,6 +26,24 @@ fn do_main() -> io::Result<()> {
     Ok(())
 }
 
+#[derive(Parser, Debug)]
+struct Cli {
+    /// The directory to traverse
+    #[arg(value_name = "DIR", help = "The directory to traverse")]
+    path: Option<PathBuf>,
+    #[arg(
+        short,
+        long,
+        value_name = "FILE",
+        help = "The file to which the output should be written"
+    )]
+    output: Option<PathBuf>,
+    #[arg(short, long, help = "Overwrites existing file if specified")]
+    force: bool,
+}
+
 fn main() {
-    do_main().unwrap();
+    let args = Cli::parse();
+    println!("{args:?}");
+    do_main(args.path.unwrap_or(".".into())).unwrap();
 }
